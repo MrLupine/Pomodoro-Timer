2d42784782a1c654d0a05750b341f8d67620f775
Josse Pickard <jossepickard@gmail.com>
2020-04-08 19:20:59 +0100

    started building timer

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
 let timer = 0
+let currentTime = new Date().getTime()
+let futureTime = setTimer(25)
+let timeLeft = (futureTime - currentTime) / 1000
+let minutesLeft = 25
+let secondsLeft = 00
 
-document.querySelectorAll('container__button').forEach(button, => {
-    button.addEventListener('click', () => alert('Ouch! Stop poking me!'));
+/* What we're counting up to.*/
+function setTimer(x) {
+	const minutes = 1000 * 60
+	return currentTime + (minutes * x)
+}
+
+document.querySelectorAll(".container__button").forEach(button => {
+	button.addEventListener("click", (e) => {
+		functions[e.target.id]();
+	})
 })
+
+const functions = {
+	"pomodoro": function() {
+		alert("don't poke me!")
+	},
+	"short": function() {
+		alert("don't poke me!")
+	},
+	"long": function() {
+		alert("don't poke me!")
+	},
+	"start": function() {
+		alert("don't poke me!")
+	},
+	"stop": function() {
+		alert("don't poke me!")
+	},
+	"pause": function() {
+		alert("don't poke me!")
+	},
+}
