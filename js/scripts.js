b580f39ae6e5c42a89f75c4564659f6b2ef2c273
Josse Pickard <jossepickard@gmail.com>
2020-04-11 15:21:42 +0100

    created functional timer, fixed some small errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,91 @@
-let timer = 0
-let currentTime = new Date().getTime()
-let futureTime = setTimer(25)
-let timeLeft = (futureTime - currentTime) / 1000
-let minutesLeft = 25
-let secondsLeft = 00
-
-/* What we're counting up to.*/
-function setTimer(x) {
-	const minutes = 1000 * 60
-	return currentTime + (minutes * x)
-}
-
 document.querySelectorAll(".container__button").forEach(button => {
 	button.addEventListener("click", (e) => {
-		functions[e.target.id]();
+		timerButtonsMap[`${e.target.id}`]();
 	})
 })
 
-const functions = {
-	"pomodoro": function() {
-		alert("don't poke me!")
+const timerButtonsMap = {
+	"pomodoro": () => {
+		timerFunctions.pomodoroCount += 1;
+		timerFunctions.timerType = "pomodoro";
+		document.getElementById("container__clock--timer").innerHTML = "25:00";
+		startStop(minToMil(0.1));
 	},
-	"short": function() {
-		alert("don't poke me!")
+	"short": () => {
+		timerFunctions.timerType = "short";
+		document.getElementById("container__clock--timer").innerHTML = "05:00";
+		startStop(minToMil(0.05));
 	},
-	"long": function() {
-		alert("don't poke me!")
+	"long": () => {
+		timerFunctions.pomodoroCount = 0;
+		timerFunctions.timerType = "long";
+		document.getElementById("container__clock--timer").innerHTML = "10:00";
+		startStop(minToMil(0.2));
 	},
-	"start": function() {
-		alert("don't poke me!")
+	"start": () => {
+		startStop(timerFunctions.milliseconds);
 	},
-	"stop": function() {
-		alert("don't poke me!")
+	"stop": () => {
+		stopIfStarted();
+		timerFunctions.timeReset();
+		document.getElementById("container__clock--timer").innerHTML = "00:00";
 	},
-	"pause": function() {
-		alert("don't poke me!")
+	"pause": () => {
+		stopIfStarted();
 	},
 }
+
+const stopIfStarted = () => {
+	if (timerFunctions.timerType) {
+		clearInterval(countdownTimer)
+	}
+}
+
+/*
+* Holds time left for countdown, allowing pause and stop functions.
+*/
+const timerFunctions = {
+	milliseconds: 0,
+	timerType: "",
+	pomodoroCount: 0,
+	timeReset: () => timerFunctions.milliseconds = 0
+}
+
+function minToMil(minutes) {
+	return minutes * 1000 * 60;
+}
+
+/*
+* On start or stop, calculate new future time to count up to.
+*/
+const startStop = milliseconds => {
+	const currentTime = new Date().getTime();
+	const futureTime = currentTime + milliseconds;
+	countdownTimer = setInterval(timerOutput, 1000, futureTime);
+}
+
+let timerOutput = (futureTime) => {
+	const timerType = (timerFunctions.timerType === "pomodoro") ? true : false
+	const clockTimer = document.getElementById("container__clock--timer");
+	let currentTime = new Date().getTime();
+	let countdownMilliseconds = futureTime - currentTime
+	const doubleDigitCheck = digit => digit < 10 ? `0${digit}` : digit < 0 ? "00" : `${digit}`; // if digit doesn't have a tenths place, place a zero before digit
+	let minutes = Math.floor((countdownMilliseconds + 1000) / 60000); // add one second locally to minutes so that minutes change at correct time
+	let seconds = ((countdownMilliseconds % 60000) / 1000).toFixed(0);
+	const secondsCheck = (x) => (x === "60") ? 00 : Number(x); // on 60 seconds left, display "00" instead
+	clockTimer.innerHTML = `${doubleDigitCheck(minutes)}:${doubleDigitCheck(secondsCheck(seconds))}`;
+	timerFunctions.milliseconds = countdownMilliseconds;
+	if (countdownMilliseconds < 0 && timerType) {
+	 	clearInterval(countdownTimer)
+	 	
+	 	if (timerFunctions.pomodoroCount < 5) {
+	 		timerButtonsMap["short"]()
+	 	} else {
+		timerButtonsMap["long"]()
+		}
+	 } else if (countdownMilliseconds < 0 && !timerType){
+	 	clearInterval(countdownTimer)
+	 	
+		timerButtonsMap["pomodoro"]()
+	 }
+};
